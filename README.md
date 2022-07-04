870902eaabca6cd42716a9e36550a02c43c0825f
DamianBis <daimob@gmail.com>
2022-07-04 12:39:27 +1000

    feat: create repo if it doesn't exist

--- README.md
+++ README.md
@@ -24,18 +24,13 @@ jobs:
         with:
           fetch-depth: 0
 
-      - name: Configure AWS Credentials
-        uses: aws-actions/configure-aws-credentials@v1.0.3
-        with:
-          aws-access-key-id: ${{ secrets.TEST_AWS_ACCESS_KEY_ID }}
-          aws-secret-access-key: ${{ secrets.TEST_AWS_SECRET_ACCESS_KEY }}
-          aws-region: us-east-1
-
       - name: Sync up to CodeCommit
-        uses: tsgit18/sync-up-to-codecommit-action@v1
+        uses: DamianBis/sync-up-to-codecommit-action@v1
         with:
           repository_name: test_repo
           aws_region: us-east-1
+          aws-access-key-id: ${{ secrets.TEST_AWS_ACCESS_KEY_ID }}
+          aws-secret-access-key: ${{ secrets.TEST_AWS_SECRET_ACCESS_KEY }}
 ```
 
 ## Inputs
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,23 @@
 
 set -ue
 
-RepositoryName="${INPUT_REPOSITORY_NAME}"
-AwsRegion="${INPUT_AWS_REGION}"
-CodeCommitUrl="https://git-codecommit.${AwsRegion}.amazonaws.com/v1/repos/${RepositoryName}"
+aws_access_key_id="${INPUT_AWS_ACCESS_KEY_ID}"
+[ -z $aws_access_key_id ] && EXPORT AWS_ACCESS_KEY_ID="$aws_access_key_id"
+
+aws_secret_access_key="${INPUT_AWS_SECRET_ACCESS_KEY}"
+[ -z $aws_secret_access_key ] && EXPORT AWS_SECRET_ACCESS_KEY="$aws_secret_access_key"
+
+aws_region="${INPUT_AWS_REGION}"
+repository_name="${INPUT_REPOSITORY_NAME}"
+
+repository=$(aws codecommit get-repository --repository-name ${repository_name} --region ${aws_region})
+
+if [ -z $repository ]
+then
+  repository=$(aws codecommit create-repository --repository-name ${repository_name} --region ${aws_region})
+fi
+
+CodeCommitUrl=$(echo $repository | jq .repositoryMetadata.cloneUrlHttp)
 
 git config --global --add safe.directory /github/workspace
 git config --global credential.'https://git-codecommit.*.amazonaws.com'.helper '!aws codecommit credential-helper $@'
